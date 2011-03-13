acc6413932b959c65c0dddcafc5ec16a5481e7d0
Nick <nick@bookn.dyndns.org>
2011-03-13 18:25:52 +0000

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ariaweb::Application.config.secret_token = 'c5838774dae7676036c9cf6ea9477230504b3276d5cd51289957904b4aae7d7e052bd69fd3d42e661c40d7720657420656912dfc828672815708722cf91a9fee'
